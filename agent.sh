d86222fbd24ec91c1371e4cc65d2881d73889e9e
root <root@vmhost02>
2018-05-14 07:59:55 +0000

    update

--- agent.sh
+++ agent.sh
@@ -3,6 +3,11 @@
 echo "Please insert syslog-ng server IP"
 read IP 
 
+if [[ $IP = "" ]];then
+	echo "reject"
+	exit 1
+fi
+
 cat << EOF >> /etc/rsyslog.conf
 # provides UDP syslog reception
 module(load="imudp")
