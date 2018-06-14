51a63270bbc53b052ba4bb2f4ec06ed1656ec5bf
root <root@vmhost02>
2018-06-14 08:15:03 +0000

    add

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --privileged -p 514:514/udp --name syslog-ng -d syslog-ng/cp
+docker run --privileged -p 514:514/udp -v /var/log/syslog-ng:/var/log/syslog-ng --name syslog-ng -d syslog-ng/cp
